e33ca338d322f4b174a6b507c2a6ba90fd9ef073
agates4 <agate97@gmail.com>
2017-04-22 12:57:09 -0400

    commit with my api keys

--- sentiment.py
+++ sentiment.py
@@ -1,10 +1,47 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly
 
+
+plotly.tools.set_credentials_file(username='caynindahouse', api_key='wIaNF2bXeUpno2BCV0FJ')
 
 tone_analyzer = ToneAnalyzerV3(
    username='4c3ca360-5df4-48d2-8a15-9546c7b08454',
    password='dYKqK7TtSwTz',
    version='2016-05-19 ')
 
-print(json.dumps(tone_analyzer.tone(text='A word is dead when it is said, some say. Emily Dickinson'), indent=2))
+sentimentJSON = tone_analyzer.tone(text='Wow, GOD DAMN! I am SO CONFIDENT!')['document_tone']['tone_categories']
+
+# emotional tones
+anger = sentimentJSON[0]["tones"][0]["score"];
+disgust = sentimentJSON[0]["tones"][1]["score"];
+fear = sentimentJSON[0]["tones"][2]["score"];
+joy = sentimentJSON[0]["tones"][3]["score"];
+sadness = sentimentJSON[0]["tones"][4]["score"];
+
+# language tone
+analytical = sentimentJSON[1]["tones"][0]["score"];
+confident = sentimentJSON[1]["tones"][1]["score"];
+tentative = sentimentJSON[1]["tones"][2]["score"];
+
+# social tones
+openness = sentimentJSON[2]["tones"][0]["score"];
+conscientiousness = sentimentJSON[2]["tones"][1]["score"];
+extraversion = sentimentJSON[2]["tones"][2]["score"];
+agreeableness = sentimentJSON[2]["tones"][3]["score"];
+emotionalRange = sentimentJSON[2]["tones"][4]["score"];
+
+data = [go.Bar(
+            x=[
+                'anger', 'disgust', 'fear', 'joy', 'sadness', 
+                'analytical', 'confident', 'tentative', 
+                'openness', 'conscientiousness', 'extraversion', 'agreeableness', 'emotional range'],
+            y=[
+                anger, disgust, fear, joy, sadness,
+                analytical, confident, tentative,
+                openness, conscientiousness, extraversion, agreeableness, emotionalRange]
+        )]
+
+py.plot(data, filename='basic-bar')
